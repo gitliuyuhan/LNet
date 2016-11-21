18947c5829e343c5d96cbe59b626aeb27b4583b2
gitliuyuhan <838532811@qq.com>
2016-11-21 19:50:29 +0800

    Woker线程

---
+++ woker.cpp
@@ -0,0 +1,50 @@
+/*======================================================
+    > File Name: woker.cpp
+    > Author: lyh
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年11月21日 星期一 18时52分28秒
+ =======================================================*/
+
+#include "woker.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+using namespace lnet;
+
+Woker::Woker():_wokerLoop(NULL)
+{
+    int ret = ::socketpair(PF_UNIX,SOCK_STREAM,0,_pipefd);
+    assert(ret == 0);
+    _pipe1Handler.reset(new IOEventHandler(_pipefd[1],EV_READ));
+    _wokerLoop.addIOHandler(_pipe1Handler);
+}
+
+Woker::~Woker()
+{}
+
+EventLoop* Woker::start()
+{
+    //创建线程
+    _thread.reset(new std::thread(&Woker::threadFunc,this));
+    {
+        std::lock_guard<std::mutex>   locker(_mutex);
+        while(_wokerLoop == NULL)
+        {
+            _cond.wait();
+        }
+    }
+    return _wokerLoop;
+}
+
+void Woker::threadFunc()
+{
+    EventLoop    loop;
+    {//保证Loop能先启动
+        std::lock_guard<std::mutex>   locker(this->_mutex);
+        _wokerLoop = &loop;
+        this->_cond.notify_one();
+    }
+    loop.waitEvent();
+}
---
+++ woker.h
@@ -0,0 +1,44 @@
+/*======================================================
+    > File Name: woker.h
+    > Author: lyh
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年11月21日 星期一 18时29分27秒
+ =======================================================*/
+
+#ifndef WORKER_H
+#define WORKER_H
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include "event_loop.h"
+
+namespace lnet
+{
+/*
+ * Woker线程
+ */
+class Woker
+{
+public:
+    Woker();
+    ~Woker();
+    //线程函数
+    void threadFunc();
+    void start();
+
+    int                 _pipefd[2];//用于和master通信
+    IOEventHandlerPtr   _pipe1Handler;
+private:
+    std::unique_ptr<std::thread>     _thread;
+    EventLoop*                       _wokerLoop;
+    std::mutex                       _mutex;
+    std::condition_variable          _cond;
+};
+
+}
+
+#endif
